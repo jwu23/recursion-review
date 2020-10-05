7aea177aa0c9e658aea1cf857ab286fd9a8aaa29
jwu23 <jasonwu1623@gmail.com>
2020-10-05 15:54:39 -0700

    All tests for stringifyJSON passed

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,34 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  //if number then return string version
+  //if null return string version
+  //boolean return string version
+  //if string return string version
+  //if array
+  //if object
+
+  if (typeof(obj) === 'string') {
+    return '"' + obj + '"';
+  }
+  if (Array.isArray(obj)) {
+    var stringified = [];
+    for (var i = 0; i < obj.length; i++) {
+      stringified.push(stringifyJSON(obj[i]));
+    }
+    return '[' + stringified.join(',') + ']';
+  }
+  if (typeof(obj) === 'object' && obj !== null) {
+    var keys = Object.keys(obj);
+    var stringifiedObj = [];
+    for (var i = 0; i < keys.length; i++) {
+      if (typeof (obj[keys[i]]) === 'function' || obj[keys[i]] === undefined) {
+        continue;
+      }
+      stringifiedObj.push(stringifyJSON(keys[i]) + ':' + stringifyJSON(obj[keys[i]]));
+    }
+    return '{' + stringifiedObj.join(',') + '}';
+  }
+
+  return '' + obj;
 };
